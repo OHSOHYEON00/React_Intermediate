140c08dc1351e1120a406a575001b2c611e6ea1d
OHSOHYEON00 <ohsohyeon24@gmail.com>
2023-11-29 20:19:29 +0100

    fix: integ react18 - ReactDOM

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import ReactDOM from "react-dom";
 import App from "App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
-
-// const container = document.getElementById("root");
+const container = document.getElementById("root");
 // const root = createRoot(container!); // createRoot(container!) if you use TypeScript
 // root.render(<App />);
+
+if (container) {
+  const root = createRoot(container);
+  root.render(<App />);
+}
